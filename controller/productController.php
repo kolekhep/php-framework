3f3d8c66907de7768c623209e3b9db6692c267f6
1277-07 <1277-07@labs.mhcc.cc.or.us>
2017-05-02 20:10:56 -0700

    Add product structure

---
+++ controller/productController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+# Include html header
+include( APP_VIEW . '/header.php' );
+
+# Include main navigation
+include( APP_VIEW . '/nav.php' );
+
+switch ( $route->getAction() ) {
+
+    case 'home':
+        $sql = "SELECT *
+                FROM product
+                ORDER BY name";
+
+        $result = $mysql->query($sql);
+
+        while ($row =  $result->fetch_assoc()) {
+          print_r($row);
+          exit;
+        }
+
+
+        include( APP_VIEW .'/product/productSubNav.php' );
+        include( APP_VIEW .'/product/homeView.php' );
+        break;
+
+    default:
+        include( APP_VIEW .'/product/productSubNav.php' );
+        include( APP_VIEW .'/product/homeView.php' );
+        break;
+}
+
+
+# Include html footer
+include( APP_VIEW . '/footer.php' );
---
+++ view/product/homeView.php
@@ -0,0 +1,8 @@
+
+        <!-- page content -->
+        <div class="col-md-9">
+          <div class="well pageContent">
+            Product
+          </div>
+        </div>
+        <!-- end page content -->
